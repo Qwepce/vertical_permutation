c9d038511f5870f5fd0478c6b37897bdde890611
Kirill Zinin <5pacejamm@mail.ru>
2024-09-23 15:27:50 +0300

    initial commit

---
+++ src/Decrypt.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Decrypt {
+    public static void main(String[] args) throws IOException {
+        String cryptoFilePath = "crypto.txt";
+        String keyFile = "key.txt";
+        String decryptFilePath = "decrypt.txt";
+
+        String solutionCryptoFilePath = "solutionMessage.txt";
+        String solutionKeyFile = "solutionKey.txt";
+        String solutionDecryptFilePath = "SolutionDecrypt.txt";
+
+        String encryptedMessage = encryptMessage(solutionCryptoFilePath);
+        List<Integer> orderOfReading = Encrypt.readOrderOfReadingCols(solutionKeyFile);
+
+        System.out.println(encryptedMessage.length());
+        System.out.println(orderOfReading.size());
+
+        List<String> result = formTable(encryptedMessage, orderOfReading);
+//
+        decryptMessage(solutionDecryptFilePath, result);
+
+    }
+
+    private static void decryptMessage(String decryptFilePath, List<String> result) throws IOException {
+        try {
+            Files.write(Paths.get(decryptFilePath), result);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String encryptMessage(String filePath) throws IOException {
+        StringBuilder sb = new StringBuilder();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    private static List<String> formTable(String encryptedMessage, List<Integer> orderOfReading) {
+        int rowNum = encryptedMessage.length() / orderOfReading.size();
+        int colNum = encryptedMessage.length() / rowNum;
+
+        List<String> resultedString = new ArrayList<>();
+
+        char[][] symbols = new char[rowNum][colNum];
+        int idx = 0;
+
+        for(int col : orderOfReading) {
+            for(int row = 0; row < rowNum; row++) {
+                symbols[row][col] = encryptedMessage.charAt(idx++);
+            }
+        }
+        int counter = 0;
+
+        for(char[] chars : symbols) {
+            StringBuilder sb = new StringBuilder();
+            for(char c : chars) {
+                sb.append(c);
+            }
+            if(counter % 2 == 0) {
+                resultedString.add(sb.reverse().toString());
+            } else {
+                resultedString.add(sb.toString());
+            }
+            counter++;
+        }
+
+        return resultedString;
+    }
+
+}
---
+++ src/Encrypt.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Encrypt {
+
+    public static void main(String[] args) throws IOException {
+
+        // Формирование таблицы с учетом задания
+        List<String> orderOfStrings = formArray("message.txt");
+        // Получение порядка считывания столбцов
+        List<Integer> orderOfReadingColumns = readOrderOfReadingCols("key.txt");
+
+        encryptText(orderOfStrings, orderOfReadingColumns);
+    }
+
+    private static void encryptText(List<String> orderOfStrings, List<Integer> orderOfReading){
+        StringBuilder sb = new StringBuilder();
+        String filePath = "crypto.txt";
+
+        for(int col : orderOfReading){
+            for(String row : orderOfStrings){
+                sb.append(row.charAt(col));
+            }
+        }
+
+        try(FileWriter writer = new FileWriter(filePath)) {
+            writer.write(sb.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<String> formArray(String filePath) throws IOException {
+        List<String> fileStrings = new ArrayList<>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
+            String line;
+            int i = 0;
+
+            while((line = br.readLine()) != null){
+                StringBuilder sb = new StringBuilder(line);
+                if(i % 2 == 0){
+                    fileStrings.add(sb.reverse().toString());
+                } else {
+                    fileStrings.add(line);
+                }
+                i++;
+            }
+        }
+
+        return fileStrings;
+    }
+
+    protected static List<Integer> readOrderOfReadingCols(String filePath) throws IOException {
+        List<Integer> orderOfReading = new ArrayList<>();
+
+        try(BufferedReader br = new BufferedReader(new FileReader(filePath))){
+            String line;
+
+            while((line = br.readLine()) != null){
+                String[] chars = line.split(", ");
+                for(String stringNumber : chars){
+                    orderOfReading.add(Integer.parseInt(stringNumber));
+                }
+            }
+        }
+
+        return orderOfReading;
+    }
+
+}
